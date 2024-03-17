b5e1eea4af23509c3fd872d88aee9603c3f4c7e7
itakash1 <itakashh11@gmail.com>
2024-03-17 23:44:53 +0400

    fix some bugs

--- js/core.js
+++ js/core.js
@@ -1,39 +1,43 @@
 
 
-function getRandomColor() {
-  const letters = '0123456789ABCDEF';
-  var color = '#';
-  for (var i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
+// function getRandomColor() {
+//   const letters = '0123456789ABCDEF';
+//   var color = '#';
+//   for (var i = 0; i < 6; i++) {
+//       color += letters[Math.floor(Math.random() * 16)];
+//   }
+//   return color;
+// }
+
+// const nextColorBtn = document.getElementById('nextColorBtn');
+
+// const previousColorBtn = document.getElementById('previousColorBtn');
+
+// var color;
+// let counter = 1;
+// const idk = nextColorBtn.onclick = function(){
+
+//   color = getRandomColor();
+
+//   document.body.style.backgroundColor = color;
+//   document.getElementById('colorCodeText').textContent = color;
+//   counter += 1;
+//   sessionStorage.setItem(counter, color);
+// }
+
+
+// previousColorBtn.onclick = function(){
+//   const savedColor = sessionStorage.getItem(counter - 1)
+
+//   document.body.style.backgroundColor = savedColor;
+//   document.getElementById('colorCodeText').textContent = savedColor;
+// }
+
+const getHiddenCard = (numCard, blureCount = 4) => {
+  const blure = ('*'.repeat(blureCount));
+  const card = numCard.replace(numCard.slice(0, 12), blure);
+  console.log(card);
 }
 
-const nextColorBtn = document.getElementById('nextColorBtn');
-
-const previousColorBtn = document.getElementById('previousColorBtn');
-
-var color;
-let counter = 1;
-const idk = nextColorBtn.onclick = function(){
-
-  color = getRandomColor();
-
-  document.body.style.backgroundColor = color;
-  document.getElementById('colorCodeText').textContent = color;
-  counter += 1;
-  sessionStorage.setItem(counter, color);
-}
-
-
-previousColorBtn.onclick = function(){
-  const savedColor = sessionStorage.getItem(counter - 1)
-
-  document.body.style.backgroundColor = savedColor;
-  document.getElementById('colorCodeText').textContent = savedColor;
-}
-
-
-
-
+getHiddenCard('1234123412344321');
 
